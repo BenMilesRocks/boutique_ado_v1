08985ab3e8a6222fc0c4197aa2011717fa1e3613
Ben Miles <benmilesGM@gmail.com>
2025-04-08 11:13:51 +0100

    adds Product models to admin.py

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,6 @@
+'''Products Admin - register Product models here!'''
 from django.contrib import admin
+from .models import Product, Category
 
-# Register your models here.
+admin.site.register(Product)
+admin.site.register(Category)
