55d1fcbadd73731b737395d76d2a4da746a72f8b
Ben Miles <benmilesGM@gmail.com>
2025-04-09 13:37:09 +0100

    adds product_detail to product/views.py

--- products/views.py
+++ products/views.py
@@ -1,5 +1,5 @@
 '''Products views'''
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 def all_products(request):
@@ -12,3 +12,14 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html', context)
+
+def product_detail(request, product_id):
+    '''Returns details for single product'''
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'products' : product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
