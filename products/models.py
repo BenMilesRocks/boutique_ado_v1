fec2b9ba5f9e5c3fa691c170c58bb325ec048b8a
Ben Miles <benmilesGM@gmail.com>
2025-04-09 11:27:22 +0100

    adds Meta class to Category

--- products/models.py
+++ products/models.py
@@ -3,6 +3,11 @@ from django.db import models
 
 class Category(models.Model):
     '''Top Level Category model'''
+
+    class Meta:
+        '''Fixes the pluralisation on the Admin page, shows Categories instead of Categorys'''
+        verbose_name_plural = 'Categories'
+
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
