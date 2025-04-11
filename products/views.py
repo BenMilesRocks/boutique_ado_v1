9f52364cfda11b5921303d7d5a52d82e8a738e91
Ben Miles <benmilesGM@gmail.com>
2025-04-11 09:52:34 +0100

    adds search query to all_products in products/views

--- products/views.py
+++ products/views.py
@@ -1,5 +1,8 @@
 '''Products views'''
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse
+from django.contrib import messages
+from django.db.models import Q
 from .models import Product
 
 def all_products(request):
@@ -7,8 +10,26 @@ def all_products(request):
 
     products = Product.objects.all() # pylint: disable=E1101
 
+    #Sets search term to None, preventing issues if no search entered
+    query = None
+
+    if request.GET:
+        # Check for search query, named 'q' in base.html
+        if 'q' in request.GET:
+            query = request.GET['q']
+            # if search query blank
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('products'))
+
+            #Using Django 'Q' to filter search results
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            #Pass filter to Products
+            products = products.filter(queries)
+
     context = {
         'products' : products,
+        'search_term' : query,
     }
 
     return render(request, 'products/products.html', context)
