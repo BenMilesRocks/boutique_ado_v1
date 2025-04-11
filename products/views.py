0bcb70940da306f9331ac29eb4b3f169354c0efa
Ben Miles <benmilesGM@gmail.com>
2025-04-11 10:12:41 +0100

    adds category filter in all_products, products/views

--- products/views.py
+++ products/views.py
@@ -3,17 +3,28 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product
+from .models import Product, Category
 
 def all_products(request):
     '''Returns all product pages, including Sorting and Searching'''
 
+    # Get all products from database
     products = Product.objects.all() # pylint: disable=E1101
 
-    #Sets search term to None, preventing issues if no search entered
+    #Sets filter variables to None, preventing issues if not entered
     query = None
+    categories = None
 
     if request.GET:
+        # Check for category in response
+        if 'category' in request.GET:
+            #Split into list, divided by commas
+            categories = request.GET['category'].split(',')
+            #Filter products by items in list
+            products = products.filter(category__name__in=categories)
+            #Filter Category by selected categories
+            categories = Category.objects.filter(name__in=categories) # pylint: disable=E1101
+
         # Check for search query, named 'q' in base.html
         if 'q' in request.GET:
             query = request.GET['q']
@@ -30,6 +41,7 @@ def all_products(request):
     context = {
         'products' : products,
         'search_term' : query,
+        'current_categories' : categories,
     }
 
     return render(request, 'products/products.html', context)
