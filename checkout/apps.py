46b0c043e062fbe31ff36b5987aa2b72a4cb97e8
Ben Miles <benmilesGM@gmail.com>
2025-05-06 16:36:35 +0100

    adds signal path to checkout/apps.py

--- checkout/apps.py
+++ checkout/apps.py
@@ -1,6 +1,11 @@
+'''Checkout Apps'''
 from django.apps import AppConfig
 
 
 class CheckoutConfig(AppConfig):
+    '''Config class for Checkout app'''
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'checkout'
+
+    def ready(self):
+        import checkout.signals #pylint: disable= W0611, C0415
