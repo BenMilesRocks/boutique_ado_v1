c771d6b08d97da51c3ab1c90f37a91c23180fa07
Ben Miles <benmilesGM@gmail.com>
2025-05-06 12:13:09 +0100

    updates save on OrderLineItem()

--- checkout/models.py
+++ checkout/models.py
@@ -30,18 +30,18 @@ class Order(models.Model):
     def _generate_order_number(self):
         '''Generate random, unique order no using UUID'''
         return hex(uuid.uuid4).upper()
-    
+
     def update_total(self):
         '''
         Update Grand Total each time a line item is added,
         accounting for delivery costs
         '''
-        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum']
+        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum'] # pylint: disable=E1101
         if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
             self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
         else:
             self.delivery_cost = 0
-        
+
         self.grand_total = self.order_total + self.delivery_cost
         self.save()
 
@@ -62,3 +62,13 @@ class OrderLineItem(models.Model):
     product_size = models.CharField(max_length=2, null=True, blank=True) # XS, S, M, L, XL
     quantity = models.IntegerField(null=False, blank=False, default=0)
     lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
+
+    def save(self, *args, **kwargs):
+        '''
+        Override original save method to set the lineitem total
+        and update the order total
+        '''
+
+        self.lineitem_total = self.product.price * self.quantity # pylint: disable=E1101
+
+        super().save(*args, **kwargs)
