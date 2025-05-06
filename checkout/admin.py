79c5c94d4f057746cbd7724d6fb5f75ceae70a97
Ben Miles <benmilesGM@gmail.com>
2025-05-06 16:03:05 +0100

    adds OrderAdmin class to checkout/admin.py

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,28 @@
-from django.contrib import admin
+'''Checkout Admin'''
 
-# Register your models here.
+from django.contrib import admin
+from .models import Order, OrderLineItem
+
+
+class OrderAdmin(admin.ModelAdmin):
+    '''Admin class for Order models'''
+    readonly_fields = (
+        'order_number', 'date', 'delivery_cost',
+        'order_total','grand_total'
+        )
+
+    fields = (
+        'order_number', 'date', 'full_name',
+        'email', 'phone_number', 'country',
+        'postcode', 'town_or_city', 'street_address1',
+        'street_address2', 'county', 'delivery_cost',
+        'order_total', 'grand_total'
+    )
+
+    list_display = (
+        'order_number', 'date', 'full_name',
+        'delivery_cost', 'order_total', 'grand_total'
+    )
+
+    ordering = '-date'
+    # orders by date in reverse order, most recent first
