44d6dacc95da59e8d94edd6bcca0a8e152f3a47a
Ben Miles <benmilesGM@gmail.com>
2025-05-12 09:57:55 +0100

    adds checkout to checkout/views.py

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+'''Checkout Views'''
+from django.shortcuts import redirect, render
+from django.urls import reverse
+from django.contrib import messages
+from .forms import OrderForm
 
-# Create your views here.
+
+def checkout(request):
+    '''Checkout view. Pulls bag from session, else displays error'''
+    bag = request.session.get('bag', {})
+    if not bag:
+        # If bag empty display error
+        messages.error(request, "There's nothing in your bag!")
+        # Redirect back to products, preventing users from entering URL
+        # to access the Checkout Page without adding products to bag
+        return redirect(reverse('products'))
+
+    order_form = OrderForm()
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form' : order_form,
+
+    }
+
+    return render(request, template, context)
